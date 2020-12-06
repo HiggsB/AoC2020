c3e800f095f4f52d1add331d3c2ccddfd4213554
Seth Troxler <sltroxler@hotmail.com>
2020-12-06 18:33:42 -0500

    true up

--- day5/solve.py
+++ day5/solve.py
@@ -8,6 +8,7 @@ for i in range(len(inp)):
     inp[i] = inp[i].rstrip()
 
 max_seat_id = 0
+possible_seats = list(range(8, 849))
 for seat in inp:
     min_row = 0
     max_row = 127
@@ -40,5 +41,7 @@ for seat in inp:
     seat_id = row * 8 + col
     if seat_id > max_seat_id:
         max_seat_id = seat_id
+    possible_seats.remove(seat_id)
 #    print("Seat: {}, Row: {}, Col: {}, ID: {}".format(seat, row, col, seat_id))
 print("Part One: Max Seat ID - {}".format(max_seat_id))
+print(possible_seats)
