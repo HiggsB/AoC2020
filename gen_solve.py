a6d72b9d80ee841c86a91fbcced375c5fb0c8385
Seth Troxler <sltroxler@hotmail.com>
2020-12-06 17:28:22 -0500

    update solve template

--- gen_solve.py
+++ gen_solve.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-inp = open("test_input", "r").readlines()
-#inp = open("input", "r").readlines()
+with open("test_input", "r") as f:
+#with open("input", "r") as f:
+    inp = f.readlines()
 
 for i in range(len(inp)):
-    inp[i] = inp[i][:-1]
+    inp[i] = inp[i].rstrip()
+
+print(inp)
