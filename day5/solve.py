dd356820e91eedc233198d243eb4a8fd4da5d131
Seth Troxler <sltroxler@hotmail.com>
2020-12-06 17:52:34 -0500

    solved d5p1!

--- day5/solve.py
+++ day5/solve.py
@@ -1,7 +1,44 @@
 #!/usr/bin/python3
 
-inp = open("test_input", "r").readlines()
-#inp = open("input", "r").readlines()
+#with open("test_input", "r") as f:
+with open("input", "r") as f:
+    inp = f.readlines()
 
 for i in range(len(inp)):
-    inp[i] = inp[i][:-1]
+    inp[i] = inp[i].rstrip()
+
+max_seat_id = 0
+for seat in inp:
+    min_row = 0
+    max_row = 127
+    min_col = 0
+    max_col = 7
+    row = 0
+    col = 0
+    for char in seat:
+        if char == "F":
+            if max_row - min_row == 1:
+                row = min_row
+            else:
+                max_row = max_row - round((max_row - min_row)/2)
+        elif char == "B":
+            if max_row - min_row == 1:
+                row = max_row
+            else:
+                min_row = min_row + round((max_row - min_row)/2)
+        elif char == "L":
+            if max_col - min_col == 1:
+                col = min_col
+            else:
+                max_col = max_col - round((max_col - min_col)/2)
+        elif char == "R":
+            if max_col - min_col == 1:
+                col = max_col
+            else:
+                min_col = min_col + round((max_col - min_col)/2)
+
+    seat_id = row * 8 + col
+    if seat_id > max_seat_id:
+        max_seat_id = seat_id
+#    print("Seat: {}, Row: {}, Col: {}, ID: {}".format(seat, row, col, seat_id))
+print("Part One: Max Seat ID - {}".format(max_seat_id))
