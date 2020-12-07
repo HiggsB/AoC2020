69f666b3ef71d34bd1c80c31180248e3b817c2a2
Seth Troxler <sltroxler@hotmail.com>
2020-12-06 19:27:06 -0500

    solved d6p2!

--- day6/solve.py
+++ day6/solve.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python3
 
-with open("test_input", "r") as f:
-#with open("input", "r") as f:
-    inp = f.readlines()
+from collections import Counter
 
-for i in range(len(inp)):
-    inp[i] = inp[i].rstrip()
+#with open("test_input", "r") as f:
+with open("input", "r") as f:
+    inp = f.read().split("\n\n")
 
-print(inp)
+part1_inp = inp.copy()
+for i in range(len(part1_inp)):
+    part1_inp[i] = inp[i].replace("\n", "")
+
+#print(inp)
+
+num_yesses = 0
+
+for group in part1_inp:
+    uniq = set(group)
+#    print(len(uniq))
+    num_yesses += len(uniq)
+print("Part One: Number of Yesses - {}".format(num_yesses))
+
+num_yesses = 0
+for group in inp:
+    seen = []
+    num_users = 0
+    for user in group.rstrip().split("\n"):
+        num_users += 1
+        for ans in user:
+            seen.append(ans)
+    c = dict(Counter(seen))
+    for val in c:
+        if c[val] == num_users:
+            num_yesses += 1
+print("Part Two: Number of Yesses - {}".format(num_yesses))
